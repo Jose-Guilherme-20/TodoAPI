0dab4400c1419d0ddc828200567395adb7271c33
José-Guilherme-20 <jgscosta20@gmail.com>
2023-02-25 21:14:29 -0300

    created  endpoints get and post  controller

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -6,13 +6,18 @@ const all = async (req, res) => {
   res.json({ allList });
 };
 const add = async (req, res) => {
-  const { title, done } = req.body;
-  let addTarefa = todo.create({
-    title: title,
-    done: done,
-  });
-  await addTarefa.save();
-  res.json({});
+  const { title } = req.body;
+  if (title) {
+    let addTarefa = await todo.create({
+      title: title,
+    });
+
+    res.status(201);
+    await addTarefa.save();
+    res.json({ item: addTarefa });
+  } else {
+    res.json({ error: "tarefa não foi criada" });
+  }
 };
 const update = async (req, res) => {};
 const remove = async (req, res) => {};
