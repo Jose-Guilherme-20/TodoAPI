8a25aa3a5da6c57248b5ec0954bcb4cb8e05c7fa
José-Guilherme-20 <jgscosta20@gmail.com>
2023-02-27 20:22:21 -0300

    created endpoints update and delete

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -1,14 +1,14 @@
-const todo = require("../models/todo");
+const Todo = require("../models/Todo");
 
 const all = async (req, res) => {
-  const allList = await todo.findAll();
+  const allList = await Todo.findAll();
   res.status(200);
   res.json({ allList });
 };
 const add = async (req, res) => {
   const { title } = req.body;
   if (title) {
-    let addTarefa = await todo.create({
+    let addTarefa = await Todo.create({
       title: title,
     });
 
@@ -19,8 +19,39 @@ const add = async (req, res) => {
     res.json({ error: "tarefa não foi criada" });
   }
 };
-const update = async (req, res) => {};
-const remove = async (req, res) => {};
+const update = async (req, res) => {
+  let id = req.params.id;
+  let todo = await Todo.findByPk(id);
+  if (todo) {
+    if (req.body.title) {
+      todo.title = req.body.title;
+    }
+    if (req.body.done) {
+      switch (req.body.done.toLowerCase()) {
+        case "true":
+        case "1":
+          todo.done = true;
+          break;
+        case "false":
+        case "0":
+          todo.done = false;
+          break;
+      }
+    }
+    await todo.save();
+    res.json({
+      item: todo,
+    });
+  }
+};
+const remove = async (req, res) => {
+  let id = req.params.id;
+  let todo = await Todo.findByPk(id);
+  if (todo) {
+    await todo.destroy();
+  }
+  res.json({});
+};
 
 module.exports = {
   all,
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,6 @@ const todoController = require("../controllers/todoController");
 router.get("/todo", todoController.all);
 router.post("/todo", todoController.add);
 router.put("/todo/:id", todoController.update);
-router.delete("/todo:id", todoController.remove);
+router.delete("/todo/:id", todoController.remove);
 
 module.exports = router;
